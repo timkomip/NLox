b81d5ec2c4e4f5f3206f52531220734d3efff5c2
Michael Timko <timkomip@gmail.com>
2021-11-06 16:35:50 -0400

    Add expression records + visitor interface

---
+++ NLox/Expressions.cs
@@ -0,0 +1,14 @@
+﻿namespace NLox
+{
+    public abstract record Expr()
+    {
+        public R Accept<R>(IVisitor<R> visitor)
+        {
+            return visitor.Visit(this);
+        }
+    };
+    public record Binary(Expr Left, Token Op, Expr Right) : Expr;
+    public record Grouping(Expr expression) : Expr;
+    public record Literal(object Value) : Expr;
+    public record Unary(Token Op, Expr Right) : Expr;
+}
---
+++ NLox/Visitor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLox
+{
+    public interface IVisitor<R>
+    {
+        R Visit<T>(T expr) where T : Expr;
+    }
+
+    public class AstPrinter : IVisitor<string>
+    {
+        public string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        public string Visit<T>(T expr) where T : Expr
+        {
+            return expr switch
+            {
+                Binary(Expr left, Token op, Expr right) => Parenthesize(nameof(Binary), left, right),
+                Grouping(Expr expression) => Parenthesize(nameof(Grouping), expression),
+                Literal(Expr expression) => Parenthesize(nameof(Literal), expression),
+                Unary(Token token, Expr expression) => Parenthesize(nameof(Unary), expression),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("(").Append(name);
+            foreach (var expr in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expr.Accept(this));
+            }
+
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+    }
+}
